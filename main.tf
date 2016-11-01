0c18ca3c13ba0e02c495d67edf7cff370ec782f1
Nick Santamaria <nick.santamaria@technocrat.com.au>
2016-11-01 14:17:58 +1100

    Added routes to VPC primary route tables - tests now passing.

--- main.tf
+++ main.tf
@@ -7,12 +7,16 @@ resource "aws_vpc_peering_connection" "primary2secondary" {
   peer_vpc_id = "${aws_vpc.secondary.id}"
   vpc_id = "${aws_vpc.primary.id}"
   auto_accept = true
+}
 
-  accepter {
-#    allow_remote_vpc_dns_resolution = true
-  }
+resource "aws_route" "primary2secondary" {
+  route_table_id = "${aws_vpc.primary.main_route_table_id}"
+  destination_cidr_block = "${aws_vpc.secondary.cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
+}
 
-  requester {
-#    allow_remote_vpc_dns_resolution = true
-  }
+resource "aws_route" "secondary2primary" {
+  route_table_id = "${aws_vpc.secondary.main_route_table_id}"
+  destination_cidr_block = "${aws_vpc.primary.cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
 }
