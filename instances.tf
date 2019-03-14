988cfaabea1a0d065a56667bf558381d9200a237
Nick Santamaria <nick.santamaria@previousnext.com.au>
2019-03-14 12:52:02 +1100

    Upgraded syntax using 0.12upgrade command.

--- instances.tf
+++ instances.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "primary-az1" {
-  instance_type = "${var.instance_class}"
-  ami           = "${var.ami_id}"
-  key_name      = "${var.key_name}"
-  subnet_id = "${aws_subnet.primary-az1.id}"
-  vpc_security_group_ids = ["${aws_security_group.primary-default.id}"]
+  instance_type          = var.instance_class
+  ami                    = var.ami_id
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.primary-az1.id
+  vpc_security_group_ids = [aws_security_group.primary-default.id]
 
   connection {
+    host = coalesce(self.public_ip, self.private_ip)
     type = "ssh"
     user = "ubuntu"
   }
@@ -13,19 +14,20 @@ resource "aws_instance" "primary-az1" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y nginx"
+      "sudo apt-get install -y nginx",
     ]
   }
 }
 
 resource "aws_instance" "primary-az2" {
-  instance_type = "${var.instance_class}"
-  ami           = "${var.ami_id}"
-  key_name      = "${var.key_name}"
-  subnet_id = "${aws_subnet.primary-az2.id}"
-  vpc_security_group_ids = ["${aws_security_group.primary-default.id}"]
+  instance_type          = var.instance_class
+  ami                    = var.ami_id
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.primary-az2.id
+  vpc_security_group_ids = [aws_security_group.primary-default.id]
 
   connection {
+    host = coalesce(self.public_ip, self.private_ip)
     type = "ssh"
     user = "ubuntu"
   }
@@ -33,19 +35,20 @@ resource "aws_instance" "primary-az2" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y nginx"
+      "sudo apt-get install -y nginx",
     ]
   }
 }
 
 resource "aws_instance" "secondary-az1" {
-  instance_type = "${var.instance_class}"
-  ami           = "${var.ami_id}"
-  key_name      = "${var.key_name}"
-  subnet_id = "${aws_subnet.secondary-az1.id}"
-  vpc_security_group_ids = ["${aws_security_group.secondary-default.id}"]
+  instance_type          = var.instance_class
+  ami                    = var.ami_id
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.secondary-az1.id
+  vpc_security_group_ids = [aws_security_group.secondary-default.id]
 
   connection {
+    host = coalesce(self.public_ip, self.private_ip)
     type = "ssh"
     user = "ubuntu"
   }
@@ -53,19 +56,20 @@ resource "aws_instance" "secondary-az1" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y nginx"
+      "sudo apt-get install -y nginx",
     ]
   }
 }
 
 resource "aws_instance" "secondary-az2" {
-  instance_type = "${var.instance_class}"
-  ami           = "${var.ami_id}"
-  key_name      = "${var.key_name}"
-  subnet_id = "${aws_subnet.secondary-az2.id}"
-  vpc_security_group_ids = ["${aws_security_group.secondary-default.id}"]
+  instance_type          = var.instance_class
+  ami                    = var.ami_id
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.secondary-az2.id
+  vpc_security_group_ids = [aws_security_group.secondary-default.id]
 
   connection {
+    host = coalesce(self.public_ip, self.private_ip)
     type = "ssh"
     user = "ubuntu"
   }
@@ -73,7 +77,8 @@ resource "aws_instance" "secondary-az2" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y nginx"
+      "sudo apt-get install -y nginx",
     ]
   }
 }
+
